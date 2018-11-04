eec33cf71d5c37a4cbc169a361fb58eaf7ceb41c
philiplross <philiplross@gmail.com>
2018-11-03 18:44:50 -0700

    update

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -2,39 +2,42 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    port: 3306,
-  
+  host: "localhost",
+
+  port: 3306,
+
+
+  user: "root",
+
+
+  password: "DJcorgi02",
+  database: "bamazon"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  connection.end();
+});
+
+function start() {
+
+
+  console.log("Question:");
+   inquirer
+     .prompt({
+     type: input,  
+     message: "whats the ID of your product 1-10?",
+       choices: [1,2,3,4,5,6,7,8,9,10]
+     })
     
-    user: "root",
-  
-   
-    password: "DJcorgi02",
-    database: "bamazon"
-  });
 
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    connection.end();
-  });
-
-  function start() {
+ start()
+ console.log("Answer:");
+ 
+};
 
 
-   console.log("is working");
-    inquirer
-      .prompt({
-      //  console.log("whats the ID of your product 1-10?", choices);
-      type: input,  
-      message: "whats the ID of your product 1-10?",
-        choices: [1,2]
-      })
-     
-  // trying to get it to do a question but nnext is to put in the asners inquirer comands to list what products as well 
-  };
-  
 
 
 
